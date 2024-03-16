f1a70ff7a99991ecfd01f0625d583f858504b2d6
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-03-16 17:41:17 +0100

    First commit

---
+++ src/main/java/com/jjkay03/nationsevent/NationsEvent.kt
@@ -0,0 +1,15 @@
+package com.jjkay03.nationsevent
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NationsEvent : JavaPlugin() {
+    // Plugin startup logic
+    override fun onEnable() {
+
+    }
+
+    // Plugin shutdown logic
+    override fun onDisable() {
+
+    }
+}

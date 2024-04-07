2c7bf8c65f0ac0ad954cce34795d287be31bffce
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-04-08 01:09:33 +0200

    Added meat on player death feature

---
+++ src/main/java/com/jjkay03/nationsevent/features/MeatPlayerDeath.kt
@@ -0,0 +1,34 @@
+package com.jjkay03.nationsevent.features
+
+import com.jjkay03.nationsevent.NationsEvent
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.inventory.ItemStack
+
+class MeatPlayerDeath : Listener {
+    private val config = NationsEvent.INSTANCE.config
+    private val featureEnabled: Boolean = config.getBoolean("feature-meat-player-death")
+
+    private val minMeatAmount = 8
+    private val maxMeatAmount = 12
+
+    // Create the suspicious meat item
+    private val suspiciousMeat: ItemStack = ItemStack(Material.COOKED_BEEF).apply {
+        amount = (minMeatAmount..maxMeatAmount).random() // Random amount between min and max
+        itemMeta = itemMeta?.apply { setDisplayName("§fSuspicious Meat") }
+    }
+
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent) {
+        if (featureEnabled) {
+            val player = event.entity as? Player ?: return
+            val dropLocation = player.location
+
+            // Drop suspicious meat at the player's death location
+            dropLocation.world.dropItem(dropLocation, suspiciousMeat)
+        }
+    }
+}

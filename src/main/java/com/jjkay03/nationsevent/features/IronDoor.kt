aa0a71f3e7c16d88c7479dbc3630ca576cc9fd42
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-08-02 21:53:21 +0200

    Updated plugin version to 1.21

--- src/main/java/com/jjkay03/nationsevent/features/IronDoor.kt
+++ src/main/java/com/jjkay03/nationsevent/features/IronDoor.kt
@@ -1,10 +1,12 @@
 package com.jjkay03.nationsevent.features
 
 import com.jjkay03.nationsevent.NationsEvent
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.Sound
 import org.bukkit.block.data.Openable
 import org.bukkit.block.data.Powerable
+import org.bukkit.block.data.type.Door
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
@@ -13,11 +15,11 @@ import org.bukkit.inventory.EquipmentSlot
 
 class IronDoor: Listener {
     private val config = NationsEvent.INSTANCE.config
-    private val featureEnabled: Boolean = config.getBoolean("feature-meat-player-death")
+    private val featureEnabled: Boolean = config.getBoolean("iron-door")
 
     private val openIronDoorsPermission = "nationsevent.openirondoor"
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = false)
     fun onPlayerInteract(event: PlayerInteractEvent)  {
         val player = event.player
         val block = event.clickedBlock
@@ -35,22 +37,20 @@ class IronDoor: Listener {
             && player.hasPermission(openIronDoorsPermission)
         ) {
             // Open the door
-            val blockData = block.blockData
-            if (blockData is Openable) {
-                // Open door if door closed
-                if (!blockData.isOpen) {
-                    blockData.isOpen = true
-                    block.world.playSound(block.location, Sound.BLOCK_IRON_DOOR_OPEN, 1.0f, 1.0f)
-                }
-                // Close door if door is open
-                else if (blockData.isOpen) {
-                    blockData.isOpen = false
-                    block.world.playSound(block.location, Sound.BLOCK_IRON_DOOR_CLOSE, 1.0f, 1.0f)
-                }
-                block.setBlockData(blockData, true) // Update block
-                player.swingMainHand() // Swing player hand
-                event.isCancelled = true // Cancel event
+            val blockData = block.blockData as Door
+            // Open door if door closed
+            if (!blockData.isOpen) {
+                blockData.isOpen = true
+                block.world.playSound(block.location, Sound.BLOCK_IRON_DOOR_OPEN, 1.0f, 1.0f)
             }
+            // Close door if door is open
+            else if (blockData.isOpen) {
+                blockData.isOpen = false
+                block.world.playSound(block.location, Sound.BLOCK_IRON_DOOR_CLOSE, 1.0f, 1.0f)
+            }
+            block.setBlockData(blockData, true) // Update block
+            player.swingMainHand() // Swing player hand
+            event.isCancelled = true // Cancel event
         }
     }
 }

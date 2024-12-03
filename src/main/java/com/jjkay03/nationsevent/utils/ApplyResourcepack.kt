0d345cd83524a317563669ae64d72c6099763f62
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-12-03 02:33:20 +0100

    Added support for multiple resourcepacks

--- src/main/java/com/jjkay03/nationsevent/utils/ApplyResourcepack.kt
+++ src/main/java/com/jjkay03/nationsevent/utils/ApplyResourcepack.kt
@@ -10,82 +10,123 @@ import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 import java.security.MessageDigest
 import java.net.URL
+import java.util.*
 
 class ApplyResourcepack : Listener {
     companion object {
         // variables
         val PLUGIN = NationsEvent.INSTANCE
         val CONFIG = PLUGIN.config
-        val RESOURCEPACK_ENABLED: Boolean = CONFIG.getBoolean("resourcepack-enable")
-        val RESOURCEPACK_URL: String? = CONFIG.getString("resourcepack-url")
-        var RESOURCEPACK_HASH: String? = CONFIG.getString("resourcepack-hash")
-        private val EXEMPT_PLAYERS: List<String> = CONFIG.getStringList("resourcepack-players-exempt")
+        val RESOURCEPACKS_ENABLED: Boolean = CONFIG.getBoolean("resourcepacks-enable")
+        val RESOURCEPACKS_URLS: List<String> = CONFIG.getStringList("resourcepacks-urls")
+        var RESOURCEPACKS_HASHES: List<String> = CONFIG.getStringList("resourcepacks-hashes")
+        private val EXEMPT_PLAYERS: List<String> = CONFIG.getStringList("resourcepacks-players-exempt")
 
         // Apply server pack
         fun applyPack(player: Player) {
-            if (!RESOURCEPACK_URL.isNullOrBlank() && !RESOURCEPACK_HASH.isNullOrBlank()) {
-                player.setResourcePack(RESOURCEPACK_URL, RESOURCEPACK_HASH!!, true)
+            if (RESOURCEPACKS_URLS.isNotEmpty() && RESOURCEPACKS_HASHES.isNotEmpty()) {
+                //player.setResourcePack(RESOURCEPACK_URL[0], RESOURCEPACK_HASH[0], true)
+                for (i in RESOURCEPACKS_URLS.indices) {
+                    player.addResourcePack(
+                        UUID.randomUUID(),
+                        RESOURCEPACKS_URLS[i],
+                        hexStringToByteArray(RESOURCEPACKS_HASHES[i]),
+                        "REQUIRED PACK",
+                        true
+                    )
+                }
             } else {
                 PLUGIN.logger.warning("Error sending resourcepack: URL or hash is missing in the configuration!")
             }
+        }
+
+        private fun hexStringToByteArray(hexString: String): ByteArray {
+            val len = hexString.length
+            val data = ByteArray(len / 2)
+            for (i in 0 until len step 2) {
+                val byteValue = hexString.substring(i, i + 2).toInt(16)
+                data[i / 2] = byteValue.toByte()
+            }
+            return data
         }
     }
 
     init {
         // Download the resource pack if it's enabled
-        if (RESOURCEPACK_ENABLED) downloadResourcePack()
+        if (RESOURCEPACKS_ENABLED) {
+            downloadResourcePacks()
+            val downloadedPacks = getDownloadedResourcePacks()
+            generateHashes(downloadedPacks)
+        }
     }
 
     // apply resourcepack on join
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        if (!RESOURCEPACK_ENABLED) return  // Cancel if resource pack is disabled
+        if (!RESOURCEPACKS_ENABLED) return  // Cancel if resource pack is disabled
         val player = event.player
         if (EXEMPT_PLAYERS.contains(player.name)) return  // Skip if player is in the exempt list
         applyPack(player)  // Apply pack to player
     }
 
     // Function to download resourcepack to generate hash
-    private fun downloadResourcePack() {
-        // Path to save the downloaded resource pack
-        val resourcepackFile = PLUGIN.dataFolder.resolve("downloads/resourcepack.zip")
+    private fun downloadResourcePacks() {
+        // Ensure the URLs list is not empty
+        if (RESOURCEPACKS_URLS.isEmpty()) { PLUGIN.logger.severe("Resource pack URL list is missing or empty in the configuration!"); return }
 
-        // Check if the URL is valid
-        if (RESOURCEPACK_URL.isNullOrBlank()) { PLUGIN.logger.severe("Resource pack URL is missing in the configuration!"); return }
+        RESOURCEPACKS_URLS.forEachIndexed { index, urlString ->
+            val resourcepackFile = PLUGIN.dataFolder.resolve("downloads/resourcepack${index + 1}.zip")
 
-        try {
-            // Download the resource pack from the URL
-            val url = URL(RESOURCEPACK_URL)
-            url.openStream().use { inputStream ->
-                Files.createDirectories(resourcepackFile.parentFile.toPath())  // Create directories if they don't exist
-                Files.copy(inputStream, resourcepackFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+            try {
+                // Validate the URL
+                val url = URL(urlString)
+
+                // Download the resource pack
+                url.openStream().use { inputStream ->
+                    Files.createDirectories(resourcepackFile.parentFile.toPath()) // Ensure directory exists
+                    Files.copy(inputStream, resourcepackFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+                }
+                PLUGIN.logger.info("Resource pack ${index + 1} downloaded successfully.")
+            } catch (e: Exception) {
+                PLUGIN.logger.severe("Error downloading resource pack ${index + 1} from $urlString: ${e.message}")
             }
-            PLUGIN.logger.info("Resource pack downloaded successfully.")
-            generateHash(resourcepackFile)  // Generate hash after downloading the resource pack
-        } catch (e: Exception) {
-            PLUGIN.logger.severe("Error downloading resource pack: ${e.message}")
         }
     }
 
     // Function that generates hash from pack
-    private fun generateHash(resourcepackFile: File) {
-        // Check if the resource pack file exists
-        if (!resourcepackFile.exists()) { PLUGIN.logger.severe("Resource pack file not found at: ${resourcepackFile.absolutePath}"); return }
+    private fun generateHashes(resourcepackFiles: List<File>) {
+        resourcepackFiles.forEachIndexed { index, resourcepackFile ->
+            // Check if the resource pack file exists
+            if (!resourcepackFile.exists()) {
+                PLUGIN.logger.severe("Resource pack file not found at: ${resourcepackFile.absolutePath}")
+                return@forEachIndexed
+            }
 
-        try {
-            // Generate the SHA-1 hash
-            val digest = MessageDigest.getInstance("SHA-1")
-            val fileBytes = Files.readAllBytes(resourcepackFile.toPath())
-            val hashBytes = digest.digest(fileBytes)
+            try {
+                // Generate the SHA-1 hash
+                val digest = MessageDigest.getInstance("SHA-1")
+                val fileBytes = Files.readAllBytes(resourcepackFile.toPath())
+                val hashBytes = digest.digest(fileBytes)
+                val hashString = hashBytes.joinToString("") { "%02x".format(it) }
 
-            // Save the generated hash back to the configuration
-            RESOURCEPACK_HASH = hashBytes.joinToString("") { "%02x".format(it) }
-            CONFIG.set("resourcepack-hash", RESOURCEPACK_HASH)
-            PLUGIN.saveConfig()
+                // Add the hash to the configuration
+                RESOURCEPACKS_HASHES = RESOURCEPACKS_HASHES.toMutableList().apply { add(hashString) }
+                CONFIG.set("resourcepacks-hashes", RESOURCEPACKS_HASHES)
+                PLUGIN.saveConfig()
 
-            PLUGIN.logger.info("Generated resource pack hash: $RESOURCEPACK_HASH")
-        } catch (e: Exception) {
-            PLUGIN.logger.severe("Error generating resource pack hash: ${e.message}")
+                PLUGIN.logger.info("Generated resource pack hash for pack ${index + 1}: $hashString")
+            } catch (e: Exception) {
+                PLUGIN.logger.severe("Error generating resource pack hash for file ${resourcepackFile.name}: ${e.message}")
+            }
         }
     }
+
+    // Function that get a list of all downloaded resourcepack
+    private fun getDownloadedResourcePacks(): List<File> {
+        val resourcePackDirectory = PLUGIN.dataFolder.resolve("downloads")
+        return List(RESOURCEPACKS_URLS.size) { index ->
+            resourcePackDirectory.resolve("resourcepack${index + 1}.zip")
+        }
+    }
+
 }

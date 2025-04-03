1b5c196190af170822988fd25a375f4b5ead4feb
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-04-03 16:16:38 +0200

    Created economy management functions

--- src/main/java/com/jjkay03/nationsevent/economy/EconomyUtils.kt
+++ src/main/java/com/jjkay03/nationsevent/economy/EconomyUtils.kt
@@ -8,14 +8,33 @@ import java.io.File
 
 object EconomyUtils {
 
+    // Function that returns the player balance file of a given player
+    private fun getPlayerBalanceFile(player: Player): File { return File(Saves.DIR_ECONOMY_BALANCES,"${player.uniqueId}.yml") }
+
     // Function to create player yml containing player balance
     fun createPlayerBalanceFile(player: Player, startingBalance: Int) {
         NationsEvent.INSTANCE.logger.info("Creating player balance file for ${player.name} with starting balance $startingBalance")
-        val file = File(Saves.DIR_ECONOMY_BALANCES,"${player.uniqueId}.yml")
+        val file = getPlayerBalanceFile(player)
         if (file.exists()) return // End if file exist
         val config = YamlConfiguration()
         config.set(Economy.PLAYER_BALANCE_FILE_KEY_IGN, player.name)
         config.set(Economy.PLAYER_BALANCE_FILE_KEY_BALANCE, startingBalance)
+        config.save(file)
+    }
+
+    // Function that returns the balance of a given player
+    fun getPlayerBalance(player: Player): Int {
+        val file = getPlayerBalanceFile(player)
+        val config = YamlConfiguration.loadConfiguration(file)
+        return config.getInt(Economy.PLAYER_BALANCE_FILE_KEY_BALANCE)
+    }
+
+    // Function to set a player's balance
+    fun setPlayerBalance(player: Player, newBalance: Int) {
+        val file = getPlayerBalanceFile(player)
+        val config = YamlConfiguration.loadConfiguration(file)
+        val updatedBalance = if (!Economy.ALLOW_NEGATIVE_BALANCE && newBalance < 0) 0 else newBalance
+        config.set(Economy.PLAYER_BALANCE_FILE_KEY_BALANCE, updatedBalance)
         config.save(file)
     }
 

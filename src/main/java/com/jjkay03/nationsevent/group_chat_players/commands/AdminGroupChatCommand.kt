7eadf7ce9ccf26ecd76e194260ac3f9c7b167840
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-04-20 16:48:13 +0200

    Added plan to /admingroupchat

--- src/main/java/com/jjkay03/nationsevent/group_chat_players/commands/AdminGroupChatCommand.kt
+++ src/main/java/com/jjkay03/nationsevent/group_chat_players/commands/AdminGroupChatCommand.kt
@@ -7,9 +7,24 @@ import org.bukkit.command.TabCompleter
 
 class AdminGroupChatCommand : CommandExecutor, TabCompleter {
 
+    /*
+
+    ❌ /agc coords <gc>
+    ❌ /agc chat <gc> message
+    ❌ /agc create <name> <owner> <players...>
+    ❌ /agc delete <ID>
+    ❌ /agc add <ID> <players...>
+    ❌ /agc remove <ID> <players...>
+    ❌ /agc list <player / @a>
+    ❌ /agc setowner <ID> <player>
+    ❌ /agc spy <ID / @a>
+
+     */
+
     // COMMAND
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        TODO("Not yet implemented")
+        // TODO
+        return true
     }
 
     // TAB COMPLETER

cf46ebb2dbde9327e4a6d67be0a8e6083c57b77c
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-09-08 17:00:14 +0200

    Fixed bug with enchant limiter
    Use to be able to use an illegal enchant item in off-hand due to the lack of checking off-hand slot.

--- src/main/java/com/jjkay03/nationsevent/gameplay_settings/LimitEnchant.kt
+++ src/main/java/com/jjkay03/nationsevent/gameplay_settings/LimitEnchant.kt
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerItemHeldEvent
+import org.bukkit.event.player.PlayerSwapHandItemsEvent
 import org.bukkit.inventory.ItemStack
 
 class LimitEnchant : Listener {
@@ -29,8 +30,16 @@ class LimitEnchant : Listener {
     // Check item when interact
     @EventHandler
     fun onItemInteract(event: PlayerInteractEvent) {
-        checkItem(event.player.inventory.itemInMainHand, event.player)
+        checkItem(event.player.inventory.itemInMainHand, event.player) // Check main hand
+        checkItem(event.player.inventory.itemInOffHand, event.player) // Check off-hand
         checkArmorSlotsItems(event.player) // Check player armor slots
+    }
+
+    // Check when swapping hand items
+    @EventHandler
+    fun onSwapHandItems(event: PlayerSwapHandItemsEvent) {
+        checkItem(event.mainHandItem, event.player) // Check main hand
+        checkItem(event.offHandItem, event.player) // Check off-hand
     }
 
     // Function to adjust enchantments on an item based on allowed levels
@@ -54,14 +63,14 @@ class LimitEnchant : Listener {
         }
     }
 
-    // Check if item is not air and if item is enchanted before running enchant checks
+    // Helper function to check if item is not air and if item is enchanted before running enchant checks
     private fun checkItem(item: ItemStack, player: Player) {
         if (item.type == Material.AIR) return
         if (item.enchantments.isEmpty()) return
         adjustEnchantments(item, player)
     }
 
-    // Check armor slots
+    // Helper function to check armor slots
     private fun checkArmorSlotsItems(player: Player) {
         // Get armor slots items
         val armorItems = listOf(

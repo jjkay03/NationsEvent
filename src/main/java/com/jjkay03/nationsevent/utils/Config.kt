ec902348740d455ca7187c6a2e1178d5e6976c0f
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-10-07 04:59:27 +0200

    Cleaned up config manager

--- src/main/java/com/jjkay03/nationsevent/utils/Config.kt
+++ src/main/java/com/jjkay03/nationsevent/utils/Config.kt
@@ -4,40 +4,41 @@ import com.jjkay03.nationsevent.NationsEvent
 
 class Config {
     companion object {
+        val CONFIG = NationsEvent.INSTANCE.config
 
         // MAIN
-        val EVENT_CODENAME = NationsEvent.INSTANCE.config.getString("event-codename", "NT0")
+        val EVENT_CODENAME = CONFIG.getString("event-codename", "NT0")
 
 
         // SETTINGS
-        val SETTINGS_DISABLED_CRAFTS = NationsEvent.INSTANCE.config.getBoolean("settings.disable-crafts", false)
-        val SETTINGS_LIMIT_ENCHANTS = NationsEvent.INSTANCE.config.getBoolean("settings.limit-enchants", false)
-        val SETTINGS_DISABLE_ENDER_PEARLS = NationsEvent.INSTANCE.config.getBoolean("settings.disable-ender-pearls", false)
-        val SETTINGS_DISABLE_WOLF_BREEDING = NationsEvent.INSTANCE.config.getBoolean("settings.disable-wolf-breeding", false)
-        val SETTINGS_FARM_PROTECTION = NationsEvent.INSTANCE.config.getBoolean("settings.farm-protection", false)
-        val SETTINGS_DISABLE_JOIN_LEAVE_MESSAGES = NationsEvent.INSTANCE.config.getBoolean("settings.disable-join-leave-messages", false)
+        val SETTINGS_DISABLED_CRAFTS = CONFIG.getBoolean("settings.disable-crafts", false)
+        val SETTINGS_LIMIT_ENCHANTS = CONFIG.getBoolean("settings.limit-enchants", false)
+        val SETTINGS_DISABLE_ENDER_PEARLS = CONFIG.getBoolean("settings.disable-ender-pearls", false)
+        val SETTINGS_DISABLE_WOLF_BREEDING = CONFIG.getBoolean("settings.disable-wolf-breeding", false)
+        val SETTINGS_FARM_PROTECTION = CONFIG.getBoolean("settings.farm-protection", false)
+        val SETTINGS_DISABLE_JOIN_LEAVE_MESSAGES = CONFIG.getBoolean("settings.disable-join-leave-messages", false)
 
 
         // FEATURES - Death Ban
-        val FEATURES_DEATH_BAN_ENABLE = NationsEvent.INSTANCE.config.getBoolean("features.death-ban.enable", false)
-        val FEATURES_DEATH_BAN_MESSAGE = NationsEvent.INSTANCE.config.getString("features.death-ban.message", "Your fight is over.")
-        val FEATURES_DEATH_BAN_LIGHTNING = NationsEvent.INSTANCE.config.getBoolean("features.death-ban.lightning", false)
+        val FEATURES_DEATH_BAN_ENABLE = CONFIG.getBoolean("features.death-ban.enable", false)
+        val FEATURES_DEATH_BAN_MESSAGE = CONFIG.getString("features.death-ban.message", "Your fight is over.")
+        val FEATURES_DEATH_BAN_LIGHTNING = CONFIG.getBoolean("features.death-ban.lightning", false)
         // FEATURES - Event IGNs
-        val FEATURES_EVENT_IGNS = NationsEvent.INSTANCE.config.getBoolean("features.event-igns", false)
+        val FEATURES_EVENT_IGNS = CONFIG.getBoolean("features.event-igns", false)
         // FEATURES - Worlds Time Sync
-        val FEATURES_WORLDS_TIME_SYNC_ENABLE = NationsEvent.INSTANCE.config.getBoolean("features.worlds-time-sync.enable", false)
-        val FEATURES_WORLDS_TIME_SYNC_WORLDS = NationsEvent.INSTANCE.config.getStringList("features.worlds-time-sync.worlds")
+        val FEATURES_WORLDS_TIME_SYNC_ENABLE = CONFIG.getBoolean("features.worlds-time-sync.enable", false)
+        val FEATURES_WORLDS_TIME_SYNC_WORLDS = CONFIG.getStringList("features.worlds-time-sync.worlds")
 
 
         // PLAYER GROUP CHAT
-        val PGC_ENABLED = NationsEvent.INSTANCE.config.getBoolean("player-group-chat.enable", false)
-        val PGC_SAVE_ON_SERVER_RESTART = NationsEvent.INSTANCE.config.getBoolean("player-group-chat.save-on-server-restart", false)
-        val PGC_LIMIT = NationsEvent.INSTANCE.config.getInt("player-group-chat.limit", 1)
-        val PGC_BYPASS_DISABLED_CHAT = NationsEvent.INSTANCE.config.getBoolean("player-group-chat.bypass-disabled-chat", false)
-        val PGC_COLOR = NationsEvent.INSTANCE.config.getString("player-group-chat.color", "§7")
-        val PGC_SPY_COLOR = NationsEvent.INSTANCE.config.getString("player-group-chat.spy-color", "§8")
-        val PGC_STAFF_MESSAGE_PREFIX = NationsEvent.INSTANCE.config.getString("player-group-chat.staff-msg-prefix", "§6[STAFF]")
-        val PGC_NAME_CHARACTER_LIMIT = NationsEvent.INSTANCE.config.getInt("player-group-chat.name-character-limit", 6)
+        val PGC_ENABLED = CONFIG.getBoolean("player-group-chat.enable", false)
+        val PGC_SAVE_ON_SERVER_RESTART = CONFIG.getBoolean("player-group-chat.save-on-server-restart", false)
+        val PGC_LIMIT = CONFIG.getInt("player-group-chat.limit", 1)
+        val PGC_BYPASS_DISABLED_CHAT = CONFIG.getBoolean("player-group-chat.bypass-disabled-chat", false)
+        val PGC_COLOR = CONFIG.getString("player-group-chat.color", "§7")
+        val PGC_SPY_COLOR = CONFIG.getString("player-group-chat.spy-color", "§8")
+        val PGC_STAFF_MESSAGE_PREFIX = CONFIG.getString("player-group-chat.staff-msg-prefix", "§6[STAFF]")
+        val PGC_NAME_CHARACTER_LIMIT = CONFIG.getInt("player-group-chat.name-character-limit", 6)
 
     }
 }

442315cf87e4b03f72e443e41756cbe75f02112f
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-11-13 16:55:33 +0100

    Made plugin Minecraft 1.21+
    Folia supports 1.21+ so no need for 1.20 specific code anymore.

--- src/main/java/com/jjkay03/nationsevent/settings/DisableCrafterCrafts.kt
+++ src/main/java/com/jjkay03/nationsevent/settings/DisableCrafterCrafts.kt
@@ -3,22 +3,16 @@ package com.jjkay03.nationsevent.settings
 import com.jjkay03.nationsevent.NationsEvent
 import com.jjkay03.nationsevent.Saves
 import com.jjkay03.nationsevent.utils.Config
-import com.jjkay03.nationsevent.utils.ServerType
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.CrafterCraftEvent
 
-// Separated crafters disabled crafts from regular DisabledCrafts for 1.20 support
-
 class DisableCrafterCrafts : Listener {
 
     // INITIALIZATION
     init {
-        // Only enable if Minecraft version 1.21+
-        if (ServerType.MINECRAFT_VERSION == ServerType.MinecraftVersion.V1_21) {
-            load(Config.SETTINGS_DISABLED_CRAFTS)
-        }
+        load(Config.SETTINGS_DISABLED_CRAFTS)
     }
 
     // LOAD (If enabled in config)

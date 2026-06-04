0845d88933a760ac509561e645a99a8fceed2d7b
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2026-06-04 13:03:54 +0200

    Fixed bug head equip prevention

--- src/main/java/com/jjkay03/nationsevent/settings/DisableEquipPlayerHead.kt
+++ src/main/java/com/jjkay03/nationsevent/settings/DisableEquipPlayerHead.kt
@@ -7,8 +7,8 @@ import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockDispenseArmorEvent
 import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.inventory.PlayerInventory
 
 class DisableEquipPlayerHead: Listener {
 
@@ -24,7 +24,7 @@ class DisableEquipPlayerHead: Listener {
         NationsEvent.INSTANCE.logger.info("- Loading setting: ${this::class.simpleName}")
     }
 
-    // Prevent players from equipping a player head
+    // Prevent players from equipping a player head via inventory click
     @EventHandler
     fun onEquipPlayerHead(event: InventoryClickEvent) {
         val player = event.whoClicked
@@ -32,7 +32,16 @@ class DisableEquipPlayerHead: Listener {
         // End if player has bypass perm (staff)
         if (player.hasPermission(Saves.PERM_STAFF)) return
 
-        // Prevent equip
+        // Direct place into helmet slot
         if (event.rawSlot == 5 && event.cursor.type == Material.PLAYER_HEAD) event.isCancelled = true
+
+        // Shift-click a player head from inventory
+        if (event.isShiftClick && event.currentItem?.type == Material.PLAYER_HEAD) event.isCancelled = true
+    }
+
+    // Prevent players from equipping a player head via dispenser
+    @EventHandler
+    fun onDispenserEquipPlayerHead(event: BlockDispenseArmorEvent) {
+        if (event.item.type == Material.PLAYER_HEAD) event.isCancelled = true
     }
 }
